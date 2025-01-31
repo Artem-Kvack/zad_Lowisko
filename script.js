daf3258023276de426bad1dd243b2f41518f12a4
Artem-Kvack <githubemail32@gmail.com>
2025-01-31 15:44:40 +0100

    new_update

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("clearButton").onclick = function(e) {
+
+   const temp = document.getElementsByClassName("textInput");
+
+   temp[0].value = '';
+   temp[1].value = '';
+   temp[2].value = '';
+   temp[3].value = '';
+   temp[4].value = '';
+  }
